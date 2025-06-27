3060125e1bcef79730596f099b25f11716df0cb2
Maria Clara Carvalho <mclaracarvalhoo76@gmail.com>
2025-06-27 16:27:15 -0300

    Adiciona a solucao do problema Next Round

--- codeforces/800-1000/next-round.py
+++ codeforces/800-1000/next-round.py
@@ -1,19 +1,13 @@
 # 158A
  
 n, k = map(int, input().split( ))
-pontuacao = list(map(int, input().split( )))
- 
-i = k
-next_round = k
-if pontuacao[0] == 0:
-    print(0)
-else: 
-    for pontuacao[i] in pontuacao[i:]:
-        if pontuacao[i] == pontuacao[k-1]:
-            next_round += 1
-        else: 
-            break
-        i += 1
-    print(next_round)
+pontuacoes = list(map(int, input().split( )))
 
-# PROBLEMA: Nos k primeiros numeros tiver m zeros, o código da WRONG_ANSWER
+next_round = 0
+for pontuacao in pontuacoes:
+    if pontuacao >= pontuacoes[k-1] and pontuacao > 0:
+        next_round += 1
+
+print(next_round)
+
+# Olhei a solução
