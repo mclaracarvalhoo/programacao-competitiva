57d053514fcadb377414411077ef4accf2d8e065
Maria Clara Carvalho <mclaracarvalhoo76@gmail.com>
2025-07-16 16:50:06 -0300

    Adiciona a solucao do problema Petya and Strings

--- codeforces/800-1000/petya-and-strings.py
+++ codeforces/800-1000/petya-and-strings.py
@@ -1,9 +1,11 @@
 # 112A - Petya and Strings
 
-palavra1 = str(input())
-palavra2 = str(input())
+palavra1 = input().lower()
+palavra2 = input().lower()
 
-if palavra1.upper() == palavra2.upper():
-    print("0")
-else:
-    print("nao sei como fazer")
+if palavra1 == palavra2:
+    print('0')
+if palavra1 > palavra2:
+    print('1')
+if palavra1 < palavra2:
+    print('-1')
